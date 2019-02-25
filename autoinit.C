5628ddb83be8a92c72c7649b6648b756bfdf07eb
Marc Espie <espie@nerim.net>
2019-02-25 17:42:53 +0100

    more const

--- autoinit.C
+++ autoinit.C
@@ -1,52 +1,29 @@
 /* autoinit.c */
 
-/* emulates atexit functionality, but much more reliable */
-
 #include "defs.h"
 #include "extern.h"
 #include "autoinit.h"
 
-LOCAL struct clist
-	{
+LOCAL struct clist {
 	struct clist *next;
 	void (*func)(void);
-	} *list = 0;
+} *list = nullptr;
 	
 
-void at_end(void (*cleanup)(void))
-	{
-#ifdef USE_AT_EXIT
+void 
+at_end(void (*cleanup)(void))
+{
 	atexit(cleanup);
-#else
-	struct clist *new;
-
-	new = (struct clist *)malloc(sizeof(struct clist));
-	if (!new)
-		{ /* don't forget to call the latest function ! */
-		(*cleanup)();	
-		end_all("Allocation problem");
-		}
-	new->next = list;
-	new->func = cleanup;
-	list = new;
-#endif
-	}
+}
 	
-void end_all(char *fmt, ...)
-	{
+void 
+end_all(const char *fmt, ...)
+{
 	va_list al;
-#ifndef USE_AT_EXIT
-	struct clist *p;
-#endif
-	if (fmt)
-		{
+	if (fmt) {
 		va_start(al, fmt);
 		vnotice(fmt, al);
 		va_end(al);
-		}
-#ifndef USE_AT_EXIT
-	for (p = list; p; p = p->next)
-		(p->func)();			/* don't bother freeing (malloc) */
-#endif
-	exit(fmt ? EXIT_FAILURE : EXIT_SUCCESS);
 	}
+	exit(fmt ? EXIT_FAILURE : EXIT_SUCCESS);
+}
--- autoinit.h
+++ autoinit.h
@@ -1,16 +1,4 @@
-/* autoinit.h
-   vi:ts=3 sw=3
- */
-
-/* $Id: autoinit.h,v 1.2 1996/05/06 14:28:41 espie Exp espie $
- * $Log: autoinit.h,v $
- * Revision 1.2  1996/05/06 14:28:41  espie
- * *** empty log message ***
- *
- * Revision 1.1  1996/04/09 21:13:22  espie
- * Initial revision
- *
- */
+/* autoinit.h */
 /* used for decentralizing initialization/termination of various
  * system routines
  */
@@ -20,11 +8,11 @@
  * s may be 0 for normal exit. DO NOT use exit() anywhere in tracker 
  * but end_all() instead.
  */
-XT /*@exits@*/ void end_all(char *fmt, ...);
+extern void end_all(const char *fmt, ...);
 
 /* at_end(cleanup): stack cleanup to be called at program's termination
  */
-XT void at_end(void (*cleanup)(void));
+extern void at_end(void (*cleanup)(void));
 
 /* INIT_ONCE: macro for autoinitialization of routines.
  * modules that need an init routine should LOCAL void INIT = init_routine,
