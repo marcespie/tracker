e4214898f0b3d834a225ce22eea3ac9c3bf3feaf
Marc Espie <espie@nausicaa.home>
2019-03-09 14:36:54 +0100

    reformat

--- display.C
+++ display.C
@@ -325,209 +325,195 @@ disp_smooth_upvolume(unsigned, unsigned para, note note, struct channel *ch)
 	color(0);
 }
 
-LOCAL void disp_smooth_downvolume(unsigned samp, unsigned para, note note, 
-	struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_smooth_downvolume(unsigned , unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<5>("   --");
 		num3(para);
-		}
+	}
 	else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
 
-LOCAL void disp_late_start(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_late_start(unsigned, unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<5>(" lte ");
 		num3(para);
-		}
-	else
+	} else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
-LOCAL void disp_retrig(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_retrig(unsigned, unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<5>(" rtg ");
 		num3(para);
-		}
+	}
 	else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
-LOCAL void disp_note_cut(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_note_cut(unsigned, unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<5>(" cut ");
 		num3(para);
-		}
-	else
+	} else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
-LOCAL void disp_offset(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_offset(unsigned, unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<4>(" off");
-		if (ch->samp->length)
-			{
-			int percent;
-			percent = para * 25600/ch->samp->length;
+		if (ch->samp->length) {
+			int percent = para * 25600/ch->samp->length;
 			if (percent <= 105)
 				num3(percent);
 			else
 				copy<3>("???");
-			}
-		else
+		} else
 			copy<3>(empty);
 		*base++ = '%';
-		}
-	else
+	} else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
 
-LOCAL void disp_skip(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
+LOCAL void 
+disp_skip(unsigned, unsigned para, note note, struct channel *ch)
+{
 	disp_note_name(ch, note);
 	color(0);
-   if (para)
-      {
+	if (para) {
 		copy<4>("skp ");
-      num3(para);
-      }
-   else
+		num3(para);
+	}
+	else
 		copy<7>("next   ");
-   }
+}
 
-LOCAL void disp_loop(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
+LOCAL void 
+disp_loop(unsigned, unsigned para, note note, struct channel *ch)
+{
 	disp_note_name(ch, note);
 	color(0);
-   if (para == 0)
-      copy<7>("SETLOOP");
-   else
-      {
+	if (para == 0)
+		copy<7>("SETLOOP");
+	else {
 		copy<4>("LOOP");
-      num3(para+1);
-      }
-   }
+		num3(para+1);
+	}
+}
 
-LOCAL void disp_vibratoslide(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_vibratoslide(unsigned, unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<5>(" vibs");
-		if (LOW(para))
-			{
+		if (LOW(para)) {
 			*base++='-';
 			num2(LOW(para));
-			}
-		else
-			{
+		} else {
 			*base++ = '+';
 			num2(HI(para));
-			}
 		}
-	else
+	} else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
-LOCAL void disp_delay_pattern(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
+LOCAL void 
+disp_delay_pattern(unsigned, unsigned para, note note, struct channel *ch)
+{
 	disp_note_name(ch, note);
 	color(0);
-   copy<4>("DLAY");
-   num3(para);
-   }
+	copy<4>("DLAY");
+	num3(para);
+}
 
 
-LOCAL void disp_smooth_up(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_smooth_up(unsigned, unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<5>(" sth-");
 		num3(para);
-		}
-	else
+	} else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
-LOCAL void disp_smooth_down(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_smooth_down(unsigned, unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<5>(" sth+");
 		num3(para);
-		}
-	else
+	} else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
-LOCAL void disp_fastskip(unsigned samp, unsigned para, note note, struct channel *ch)
-   {
+LOCAL void 
+disp_fastskip(unsigned, unsigned para, note note, struct channel *ch)
+{
 	disp_note_name(ch, note);
 	color(0);
-   copy<4>(" ff ");
-   num3(para);
-   }
+	copy<4>(" ff ");
+	num3(para);
+}
 
-LOCAL void disp_invert_loop(unsigned samp, unsigned para, note note, struct channel *ch)
-	{
+LOCAL void 
+disp_invert_loop(unsigned, unsigned para, note note, struct channel *ch)
+{
 	disp_note_name(ch, note);
-	if (para)
-		{
+	if (para) {
 		copy<4>("inv ");
-      num3(para);
-		}
-	else
+		num3(para);
+	} else
 		copy<7>("inv off");
 	color(0);
-	}
+}
 
-LOCAL void disp_change_finetune(unsigned samp, unsigned para, note note, 
-	struct channel *ch)
-   {
-	if (ch->samp->start)
-		{
+LOCAL void 
+disp_change_finetune(unsigned, unsigned para, note note, struct channel *ch)
+{
+	if (ch->samp->start) {
 		copy<3>(note2name(note));
 		copy<6>(" fine ");
 		num2(para);
-		}
-	else
+	} else
 		copy<11>(empty);
 	color(0);
-   }
+}
 
-LOCAL void disp_vibrato_wave(unsigned samp, unsigned para, note note, struct channel *ch)
+LOCAL void 
+disp_vibrato_wave(unsigned, unsigned para, note note, struct channel *ch)
 	{
 	disp_note_name(ch, note);
 	copy<3>("vb ");
-	switch(para)
-		{
+	switch(para) {
 	case 0:
 		copy<4>("sine");
 		break;
@@ -548,16 +534,16 @@ LOCAL void disp_vibrato_wave(unsigned samp, unsigned para, note note, struct cha
 		break;
 	default:
 		copy<4>("????");
-		}
-	color(0);
 	}
+	color(0);
+}
 
-LOCAL void disp_tremolo_wave(unsigned samp, unsigned para, note note, struct channel *ch)
-	{
+LOCAL void 
+disp_tremolo_wave(unsigned, unsigned para, note note, struct channel *ch)
+{
 	disp_note_name(ch, note);
 	copy<3>("tr ");
-	switch(para)
-		{
+	switch(para) {
 	case 0:
 		copy<4>("sine");
 		break;
@@ -578,68 +564,66 @@ LOCAL void disp_tremolo_wave(unsigned samp, unsigned para, note note, struct cha
 		break;
 	default:
 		copy<4>("????");
-		}
-	color(0);
 	}
+	color(0);
+}
 
 
-LOCAL void disp_gliss_ctrl(unsigned samp, unsigned para, note note, struct channel *ch)
-	{
+LOCAL void 
+disp_gliss_ctrl(unsigned, unsigned para, note note, struct channel *ch)
+{
 	disp_note_name(ch, note);
 	if (para)
 		copy<6>("gls on");
 	else
 		copy<6>("gls off");
-	}
+}
 
-LOCAL void init_display(void)
-   {
-   unsigned int i;
-
-   for (i = 0; i < NUMBER_EFFECTS; i++)
-      table[i] = disp_nothing;
+LOCAL void 
+init_display(void)
+{
+	for (auto& x: table)
+		x = disp_nothing;
 	table[EXT_BASE] = disp_default;
-   table[EFF_SPEED] = disp_speed;
-   table[EFF_OLD_SPEED] = disp_old_speed;
-   table[EFF_PORTA] = disp_portamento;
-   table[EFF_PORTASLIDE] = disp_portaslide;
-   table[EFF_VIBRATO] = disp_vibrato;
+	table[EFF_SPEED] = disp_speed;
+	table[EFF_OLD_SPEED] = disp_old_speed;
+	table[EFF_PORTA] = disp_portamento;
+	table[EFF_PORTASLIDE] = disp_portaslide;
+	table[EFF_VIBRATO] = disp_vibrato;
 	table[EFF_TREMOLO] = disp_tremolo;
-   table[EFF_UP] = disp_upslide;
-   table[EFF_DOWN] = disp_downslide;
-   table[EFF_ARPEGGIO] = disp_arpeggio;
+	table[EFF_UP] = disp_upslide;
+	table[EFF_DOWN] = disp_downslide;
+	table[EFF_ARPEGGIO] = disp_arpeggio;
 	table[EFF_NONE] = disp_default;
-   table[EFF_VOLUME] = disp_volume;
-   table[EFF_VOLSLIDE] = disp_slidevol;
-   table[EFF_OFFSET] = disp_offset;
-   table[EFF_LATESTART] = disp_late_start;
-   table[EFF_S_UPVOL] = disp_smooth_upvolume;
-   table[EFF_S_DOWNVOL] = disp_smooth_downvolume;
-   table[EFF_SKIP] = disp_skip;
-   table[EFF_LOOP] = disp_loop;
-   table[EFF_VIBSLIDE] = disp_vibratoslide;
-   table[EFF_RETRIG] = disp_retrig;
-   table[EFF_NOTECUT] = disp_note_cut;
-   table[EFF_DELAY] = disp_delay_pattern;
-   table[EFF_SMOOTH_UP] = disp_smooth_up;
-   table[EFF_SMOOTH_DOWN] = disp_smooth_down;
-   table[EFF_FF] = disp_fastskip;
+	table[EFF_VOLUME] = disp_volume;
+	table[EFF_VOLSLIDE] = disp_slidevol;
+	table[EFF_OFFSET] = disp_offset;
+	table[EFF_LATESTART] = disp_late_start;
+	table[EFF_S_UPVOL] = disp_smooth_upvolume;
+	table[EFF_S_DOWNVOL] = disp_smooth_downvolume;
+	table[EFF_SKIP] = disp_skip;
+	table[EFF_LOOP] = disp_loop;
+	table[EFF_VIBSLIDE] = disp_vibratoslide;
+	table[EFF_RETRIG] = disp_retrig;
+	table[EFF_NOTECUT] = disp_note_cut;
+	table[EFF_DELAY] = disp_delay_pattern;
+	table[EFF_SMOOTH_UP] = disp_smooth_up;
+	table[EFF_SMOOTH_DOWN] = disp_smooth_down;
+	table[EFF_FF] = disp_fastskip;
 	table[EFF_INVERT_LOOP] = disp_invert_loop;
-   table[EFF_CHG_FTUNE] = disp_change_finetune;
+	table[EFF_CHG_FTUNE] = disp_change_finetune;
 	table[EFF_VIBRATO_WAVE] = disp_vibrato_wave;
 	table[EFF_TREMOLO_WAVE] = disp_tremolo_wave;
 	table[EFF_GLISS_CTRL] = disp_gliss_ctrl;
-   }
+}
 
-void dump_event(struct channel *ch, struct event *e)
-   {
-   
-	if (ch)	/* do we have a scroll line AND are we not finished */
-		{
+void 
+dump_event(struct channel *ch, struct event *e)
+{
+	if (ch)	{ /* do we have a scroll line AND are we not finished */
 		if (!base)
 			base = new_scroll();
-		if (base)
-			{
+		if (base) {
 			color(ch->samp->color);
 			if (ch->samp != empty_sample())
 				*base++ = instname[e->sample_number];
@@ -647,24 +631,19 @@ void dump_event(struct channel *ch, struct event *e)
 				*base++ = ' ';
 			*base++ = ' ';
 			debug = e->effect;
-			(*table[e->effect])(e->sample_number, e->parameters, e->note, ch);
-			}
+			(*table[e->effect])(e->sample_number, e->parameters, 
+			    e->note, ch);
 		}
-	else
-		{
+	} else {
 		*base = 0;
 		scroll(base);
 		base = 0;
-		}
 	}
+}
 
-void dump_delimiter(void)
-	{
-
+void 
+dump_delimiter(void)
+{
 	if (base)
-#ifdef AMIGA
-		*base++ = ' ';
-#else
 		*base++= '|';
-#endif
-	}
+}
