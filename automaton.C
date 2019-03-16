50f5e2987a941a48493237b4ae6367f611b30223
Marc Espie <espie@nausicaa.home>
2019-03-16 16:06:19 +0100

    reformat

--- automaton.C
+++ automaton.C
@@ -13,134 +13,133 @@
 /* set up the automaton so that I haven't got through patterns 
  * #from to #to
  */
-static void clear_repeats(struct automaton *a, unsigned int from, 
-	unsigned int upto)
-   {
-   unsigned int i;
-
-   for (i = from; i <= upto; i++)
-      a->gonethrough[i] = false;
-   }
+static void 
+clear_repeats(automaton *a, unsigned int from, unsigned int upto)
+{
+	for (unsigned i = from; i <= upto; i++)
+		a->gonethrough[i] = false;
+}
 
 /* set up the automaton so that I haven't got through any patterns
  */
-static void reset_repeats(struct automaton *a)
-   {
-   clear_repeats(a, 0, a->info->length);
-   a->gonethrough[a->info->length] = true;
-   }
+static void 
+reset_repeats(automaton *a)
+{
+	clear_repeats(a, 0, a->info->length);
+	a->gonethrough[a->info->length] = true;
+}
 
 /* update the pattern to play in the automaton. Checks that the pattern 
  * actually exists. Handle repetitions as well.
  */
-static void set_pattern(struct automaton *a)
-   {
-   if (a->pattern_num >= a->info->length)
-      {
-      error = UNRECOVERABLE;
-      return;
-      }
+static void 
+set_pattern(automaton *a)
+{
+	if (a->pattern_num >= a->info->length) {
+		error = UNRECOVERABLE;
+		return;
+	}
 
-   if (a->gonethrough[a->pattern_num])
-      {
-      error = ENDED;
-      reset_repeats(a);
-      }
-   else
-      a->gonethrough[a->pattern_num] = true;
+	if (a->gonethrough[a->pattern_num]) {
+		error = ENDED;
+		reset_repeats(a);
+	}
+	else
+		a->gonethrough[a->pattern_num] = true;
 
-      /* there is a level of indirection in the format,
-       * i.e., patterns can be repeated.
-       */
-   a->pattern = a->info->patterns+a->pattern_num;
+	/* there is a level of indirection in the format,
+	* i.e., patterns can be repeated.
+	*/
+	a->pattern = a->info->patterns+a->pattern_num;
 
 
-   display_pattern(a->pattern_num, a->info->length, a->pattern->number, 
+	display_pattern(a->pattern_num, a->info->length, a->pattern->number, 
 	a->pattern->total, a->info->duration);
 	if (error == ENDED)
 		display_time(a->time_spent, a->info->duration);
-   }
+}
 
 /* initialize all the fields of the automaton necessary
  * to play a given song.
  */
-static void init_automaton(struct automaton *a, struct song *song, 
-	unsigned int start)
-   {
-   a->info = &song->info;
-   a->pattern_num = start;    /* first pattern */
+static void 
+init_automaton(automaton *a, song *song, unsigned int start)
+{
+	a->info = &song->info;
+	a->pattern_num = start;    /* first pattern */
 
-   a->delay_counter = 0;
+	a->delay_counter = 0;
 
-   reset_repeats(a);
+	reset_repeats(a);
 
-   a->note_num = 0;           /* first note in pattern */
-   a->counter = 0;            /* counter for the effect tempo */
-   a->speed = NORMAL_SPEED;
-   a->finespeed = NORMAL_FINESPEED;
-										/* (100%=NORMAL_FINESPEED) */
-   a->do_stuff = DO_SET_NOTHING;  /* some effects affect the automaton,
-                               * we keep them here.  */
-   error = NONE;              /* Maybe we should not reset errors at
-                               * this point ?  */
-   a->time_spent = 0;
-   set_pattern(a);
-   }
+	a->note_num = 0;           /* first note in pattern */
+	a->counter = 0;            /* counter for the effect tempo */
+	a->speed = NORMAL_SPEED;
+	a->finespeed = NORMAL_FINESPEED;
+	/* (100%=NORMAL_FINESPEED) */
+	a->do_stuff = DO_SET_NOTHING;  /* some effects affect the automaton,
+			       		* we keep them here.  */
+	error = NONE;              /* Maybe we should not reset errors at
+			       	    * this point ?  */
+	a->time_spent = 0;
+	set_pattern(a);
+}
 
-struct automaton *setup_automaton(struct song *s, unsigned int start)
-	{
-	static struct automaton a;
+automaton *
+setup_automaton(song *s, unsigned int start)
+{
+	static automaton a;
 
 	init_automaton(&a, s, start);
 	return &a;
-	}
+}
 
 /* get to the next pattern, and display stuff 
  */
-static void advance_pattern(struct automaton *a)
-   {
-   if (++a->pattern_num >= a->info->length)
-		{
+static void 
+advance_pattern(automaton *a)
+{
+	if (++a->pattern_num >= a->info->length) {
 		error = ENDED;
 		reset_repeats(a);
-      a->pattern_num = 0;
-		}
-   set_pattern(a);
-   a->note_num = 0;
-   }
+		a->pattern_num = 0;
+	}
+	set_pattern(a);
+	a->note_num = 0;
+}
 
-void set_bpm(struct automaton *a, unsigned int bpm)
-	{
+void 
+set_bpm(automaton *a, unsigned int bpm)
+{
 	a->bpm = bpm;
 	set_resampling_beat(a->bpm, NORMAL_FINESPEED, a->finespeed);
-	}
+}
 
-void update_tempo(struct automaton *a)
-   {        
-      /* there are three classes of speed changes:
-       * 0 does nothing. (should stop for genuine protracker)
-       * <32 is the effect speed (resets the fine speed).
-       * >=32 changes the finespeed, default 125
-       */
+void 
+update_tempo(automaton *a)
+{        
+	/* there are three classes of speed changes:
+	 * 0 does nothing. (should stop for genuine protracker)
+	 * <32 is the effect speed (resets the fine speed).
+	 * >=32 changes the finespeed, default 125
+	 */
 
-	switch(a->do_stuff & (SET_SPEED | SET_FINESPEED))
-		{
+	switch(a->do_stuff & (SET_SPEED | SET_FINESPEED)) {
 	case SET_SPEED | SET_FINESPEED:
-		if (get_pref_scalar(PREF_SPEEDMODE) != FINESPEED_ONLY)
-			{
+		if (get_pref_scalar(PREF_SPEEDMODE) != FINESPEED_ONLY) {
 			a->finespeed = a->new_finespeed;
-			set_resampling_beat(a->bpm, NORMAL_FINESPEED, a->finespeed);
-			}
+			set_resampling_beat(a->bpm, NORMAL_FINESPEED, 
+			    a->finespeed);
+		}
 		if (get_pref_scalar(PREF_SPEEDMODE) != SPEED_ONLY)
 			a->speed = a->new_speed;
 		break;
 	case SET_SPEED:
 		a->speed = a->new_speed;
-		if (get_pref_scalar(PREF_SPEEDMODE) == ALTER_PROTRACKER)
-			{
+		if (get_pref_scalar(PREF_SPEEDMODE) == ALTER_PROTRACKER) {
 			a->finespeed = NORMAL_FINESPEED;
 			set_resampling_beat(a->bpm, 1, 1);
-			}
+		}
 		break;
 	case SET_FINESPEED:
 		a->finespeed = a->new_finespeed;
@@ -148,62 +147,53 @@ void update_tempo(struct automaton *a)
 		break;
 	default:
 		break;
-		}
+	}
 
-	if (a->finespeed == 0)
-		{
+	if (a->finespeed == 0) {
 		status("Finespeed of 0");
 		a->finespeed = NORMAL_FINESPEED;
 		set_resampling_beat(a->bpm, NORMAL_FINESPEED, a->finespeed);
 		error = FAULT;
-		}
-   }
+	}
+}
 
 /* process all the stuff which we need to advance in the song,
  * including set_skip, set_fastskip, and set_loop.
  */
-void next_tick(struct automaton *a)
-   {
-   a->time_spent += ratio2time(NORMAL_FINESPEED, a->finespeed * 50);
-   if (++a->counter >= a->speed)
-      {
-      a->counter = 0;
-				/* if we are in delay mode, count down delay */
+void 
+next_tick(automaton *a)
+{
+	a->time_spent += ratio2time(NORMAL_FINESPEED, a->finespeed * 50);
+	if (++a->counter >= a->speed) {
+		a->counter = 0;
+		/* if we are in delay mode, count down delay */
 		if (a->delay_counter > 0)
 			a->delay_counter--;
-			/* get to next tick ONLY if no delay */
-		if (a->delay_counter == 0)
-			{
-				/* loop: may change note in pattern right away */
+		/* get to next tick ONLY if no delay */
+		if (a->delay_counter == 0) {
+			/* loop: may change note in pattern right away */
 			if (a->do_stuff & JUMP_PATTERN)
 				a->note_num = a->loop_note_num;
-			else if (a->do_stuff & SET_FASTSKIP)
-				{
+			else if (a->do_stuff & SET_FASTSKIP) {
 				a->pattern_num = a->new_pattern;
 				set_pattern(a);
 				a->note_num = 0;
-				}
-			else if (a->do_stuff & SET_SKIP)
-				{
+			} else if (a->do_stuff & SET_SKIP) {
 				advance_pattern(a);
 				a->note_num = a->new_note;
+			} else {
+				if (++a->note_num >= a->info->plength) {
+					advance_pattern(a);
 				}
-			else
-				{
-            if (++a->note_num >= a->info->plength)
-               {
-               advance_pattern(a);
-               }
-				}
+			}
 			a->do_stuff = DO_SET_NOTHING;
-         }
-      }
-
-
-   }
-
-
-struct event *EVENT(struct automaton *a, int channel)
-	{
-	return &(a->pattern->e[channel * a->info->plength + a->note_num]);
+		}
 	}
+}
+
+
+event *
+EVENT(automaton *a, int channel)
+{
+	return &(a->pattern->e[channel * a->info->plength + a->note_num]);
+}
